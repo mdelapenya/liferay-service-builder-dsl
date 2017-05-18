594f3338a25c6c15d92f49a6aa16066d91d0a040
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-18 16:29:10 +0200

    Create default entrypoint for the application

---
+++ src/main/java/com/liferay/servicebuilder/dsl/Entrypoint.java
@@ -0,0 +1,12 @@
+package com.liferay.servicebuilder.dsl;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class Entrypoint {
+
+	public static void main(String[] args) {
+		System.out.println("Creating Liferay Service Builder!!");
+	}
+
+}

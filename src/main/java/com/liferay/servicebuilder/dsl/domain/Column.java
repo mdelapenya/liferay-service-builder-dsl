ac1e86e177ffa79e95844632148f57c9ad3d3701
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-19 07:39:10 +0200

    Define builder for Column domain model

---
+++ src/main/java/com/liferay/servicebuilder/dsl/domain/Column.java
@@ -0,0 +1,80 @@
+package com.liferay.servicebuilder.dsl.domain;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class Column {
+
+	public Column(Builder builder) {
+		_name = builder._name;
+		_type = builder._type;
+
+		_accesor = builder._accesor;
+		_filterPrimary = builder._filterPrimary;
+		_primary = builder._primary;
+	}
+
+	public boolean isAccesor() {
+		return _accesor;
+	}
+
+	public boolean isFilterPrimary() {
+		return _filterPrimary;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public boolean isPrimary() {
+		return _primary;
+	}
+
+	public String getType() {
+		return _type;
+	}
+
+	public static class Builder {
+
+		public Builder(String name, String type) {
+			_name = name;
+			_type = type;
+		}
+
+		public Builder withAccesor() {
+			_accesor = true;
+
+			return this;
+		}
+
+		public Builder filterPrimary() {
+			_filterPrimary = true;
+
+			return this;
+		}
+
+		public Builder primary() {
+			_primary = true;
+
+			return this;
+		}
+
+		public Column build() {
+			return new Column(this);
+		}
+
+		private boolean _accesor;
+		private boolean _filterPrimary;
+		private String _name;
+		private boolean _primary;
+		private String _type;
+
+	}
+
+	private boolean _accesor;
+	private boolean _filterPrimary;
+	private String _name;
+	private boolean _primary;
+	private String _type;
+
+}
---
+++ src/test/java/com/liferay/servicebuilder/dsl/domain/ColumnTest.java
@@ -0,0 +1,45 @@
+package com.liferay.servicebuilder.dsl.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class ColumnTest {
+
+	@Test
+	public void testBuild() {
+		Column column = builder.build();
+
+		Assert.assertEquals("groupId", column.getName());
+		Assert.assertEquals("long", column.getType());
+		Assert.assertFalse(column.isAccesor());
+		Assert.assertFalse(column.isFilterPrimary());
+		Assert.assertFalse(column.isPrimary());
+	}
+
+	@Test
+	public void testBuildWithAccessor() {
+		Column column = builder.withAccesor().build();
+
+		Assert.assertTrue(column.isAccesor());
+	}
+
+	@Test
+	public void testBuildFilterPrimary() {
+		Column column = builder.filterPrimary().build();
+
+		Assert.assertTrue(column.isFilterPrimary());
+	}
+
+	@Test
+	public void testBuildPrimary() {
+		Column column = builder.primary().build();
+
+		Assert.assertTrue(column.isPrimary());
+	}
+
+	private Column.Builder builder = new Column.Builder("groupId", "long");
+
+}

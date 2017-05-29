6f179c94a8aae0a818dcf87b8ec802c5bf22239b
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-29 12:28:16 +0200

    Add a Jackson serializer for reference domain model

---
+++ src/main/java/com/liferay/servicebuilder/dsl/xml/ReferenceSerializer.java
@@ -0,0 +1,14 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.liferay.servicebuilder.dsl.domain.Reference;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class ReferenceSerializer extends BaseXMLSerializer {
+
+	public ReferenceSerializer(Reference reference) {
+		super(reference);
+	}
+
+}
---
+++ src/test/java/com/liferay/servicebuilder/dsl/xml/ReferenceSerializerTest.java
@@ -0,0 +1,32 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.liferay.servicebuilder.dsl.domain.Reference;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class ReferenceSerializerTest {
+
+	@Test
+	public void testSerialize() throws JsonProcessingException {
+		Reference reference =
+			new Reference.Builder()
+				.injectService("JournalFolder", "com.liferay.journal")
+				.build();
+
+		XMLSerializer serializer = new ReferenceSerializer(reference);
+
+		String xml = serializer.serialize();
+
+		Assert.assertEquals(
+			"<reference><entity>JournalFolder</entity>" +
+				"<package-path>com.liferay.journal</package-path></reference>",
+			xml);
+	}
+
+}

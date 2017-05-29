bbc50e03eee9d6a064f4c016ad53685de13c8197
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-29 12:26:52 +0200

    Add a Jackson serializer for tx-required domain model

---
+++ src/main/java/com/liferay/servicebuilder/dsl/xml/TxRequiredMethodSerializer.java
@@ -0,0 +1,14 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.liferay.servicebuilder.dsl.domain.TxRequiredMethod;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class TxRequiredMethodSerializer extends BaseXMLSerializer {
+
+	public TxRequiredMethodSerializer(TxRequiredMethod txRequiredMethod) {
+		super(txRequiredMethod);
+	}
+
+}
---
+++ src/test/java/com/liferay/servicebuilder/dsl/xml/TxRequiredMethodSerializerTest.java
@@ -0,0 +1,30 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.liferay.servicebuilder.dsl.domain.TxRequiredMethod;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class TxRequiredMethodSerializerTest {
+
+	@Test
+	public void testSerialize() throws JsonProcessingException {
+		TxRequiredMethod txRequiredMethod =
+			new TxRequiredMethod.Builder()
+				.addMethodRequiringTxPropagation("getFoo")
+				.build();
+
+		XMLSerializer serializer = new TxRequiredMethodSerializer(
+			txRequiredMethod);
+
+		String xml = serializer.serialize();
+
+		Assert.assertEquals("<tx-required>getFoo</tx-required>", xml);
+	}
+
+}

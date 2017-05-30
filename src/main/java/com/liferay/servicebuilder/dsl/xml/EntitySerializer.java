69cfd7a98670b2deea7027cbc67c5598ad66cae4
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-05-30 08:51:34 +0200

    [#6] Support serializing entity domain model

---
+++ src/main/java/com/liferay/servicebuilder/dsl/xml/EntitySerializer.java
@@ -0,0 +1,14 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.liferay.servicebuilder.dsl.domain.Entity;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class EntitySerializer extends BaseXMLSerializer {
+
+	public EntitySerializer(Entity entity) {
+		super(entity);
+	}
+
+}
---
+++ src/test/java/com/liferay/servicebuilder/dsl/xml/EntitySerializerTest.java
@@ -0,0 +1,76 @@
+package com.liferay.servicebuilder.dsl.xml;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.liferay.servicebuilder.dsl.domain.Column;
+import com.liferay.servicebuilder.dsl.domain.Entity;
+import com.liferay.servicebuilder.dsl.domain.Finder;
+import com.liferay.servicebuilder.dsl.domain.FinderColumn;
+import com.liferay.servicebuilder.dsl.domain.Order;
+import com.liferay.servicebuilder.dsl.domain.OrderColumn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class EntitySerializerTest {
+
+	@Test
+	public void testSerialize() throws JsonProcessingException {
+		Column column = new Column.Builder("companyId", "long")
+			.asPrimaryKey()
+			.build();
+
+		OrderColumn orderColumn = new OrderColumn.Builder("companyId")
+			.descending()
+			.build();
+
+		Order order = new Order.Builder()
+			.withOrderColumn(orderColumn)
+			.build();
+
+		FinderColumn finderColumn = new FinderColumn.Builder("companyId")
+			.build();
+
+		Finder finder = new Finder.Builder("CompanyId", "Collection")
+			.withFinderColumn(finderColumn)
+			.build();
+
+		Entity entity =
+			new Entity.Builder("JournalArticle")
+				.withDynamicUpdate(true)
+				.disableCache()
+				.withLocalServices()
+				.withJsonSerialization()
+				.disableTxManager()
+				.withColumn(column)
+				.withOrder(order)
+				.withFinder(finder)
+				.build();
+
+		XMLSerializer serializer = new EntitySerializer(entity);
+
+		String xml = serializer.serialize();
+
+		Assert.assertEquals(
+			"<entity cache-enabled=\"false\" deprecated=\"false\" " +
+				"dynamic-update-enabled=\"true\" json-enabled=\"true\" " +
+				"local-service=\"true\" mvcc-enabled=\"false\" " +
+				"name=\"JournalArticle\" remote-service=\"true\" " +
+				"trash-enabled=\"false\" tx-manager=\"none\" uuid=\"false\" " +
+				"uuid-accessor=\"false\"><column accessor=\"false\" " +
+				"container-model=\"false\" convert-null=\"false\" " +
+				"filter-primary=\"false\" json-enabled=\"false\" " +
+				"lazy=\"true\" localized=\"false\" name=\"companyId\" " +
+				"parent-container-model=\"false\" primary=\"true\" " +
+				"type=\"long\"/><finder db-index=\"true\" name=\"CompanyId\" " +
+				"return-type=\"Collection\" unique=\"false\"><finder-column " +
+				"case-sensitive=\"true\" name=\"companyId\"/></finder>" +
+				"<order><order-column case-sensitive=\"true\" " +
+				"name=\"companyId\" order-by=\"DESC\"/></order></entity>",
+			xml);
+	}
+
+}
